07cad4d651e13686074d0669fed471c3513cf22e
pseex_py <pseex_py@example.com>
2017-05-20 07:33:56 +0300

    first commit

---
+++ today.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: doom
+ * Date: 19.05.2017
+ * Time: 22:52
+ */
+
+$MyArray = array('1', 2, 10);
+foreach ($MyArray as $a):
+    echo $a."<br>";
+endforeach;
+
+
+
+
+
+echo '<strong>';
+echo date("l, F jS Y.");
+echo '</strong>';
+
+$var = 1;
+
+echo "<br>";
+
+echo "var = $var";
+echo "<br>";
+echo 'var = $var';
